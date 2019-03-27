8b0569c2a22bb71dbf5929c10b38b62d1fc43c79
ha4gu <9391978+ha4gu@users.noreply.github.com>
2019-03-27 21:26:35 +0900

    Enable Action Cable and implement a basic part of it

--- app/assets/javascripts/rooms.js
+++ app/assets/javascripts/rooms.js
@@ -1,1 +1,13 @@
-alert('OK!');
+document.addEventListener('DOMContentLoaded', () => {
+  const inputarea = document.getElementById('new_post_input');
+  inputarea.focus();
+  inputarea.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      if (event.target.value !== '') {
+        App.chat.speak(event.target.value);
+        event.target.value = '';
+      };
+      event.preventDefault();
+    };
+  });
+})
